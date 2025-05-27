72f752c8bc7232ede47d21654d38a75ab7358fca
Your Nameshiafrahman <shiafrahman@gmail.com>
2025-05-28 01:38:49 +0600

    b

--- Assessment_Frontend/Models/Cart.cs
+++ Assessment_Frontend/Models/Cart.cs
@@ -1,13 +1,6 @@
 ﻿namespace Assessment_Frontend.Models
 {
-    public class Cart
-    {
-        public int Id { get; set; }
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
-        public Product Product { get; set; }
-    }
-
+    
     public class CartItem
     {
         public int ProductId { get; set; }
